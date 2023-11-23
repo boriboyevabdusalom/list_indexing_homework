9df91630dbadcbed45b1f209acdbf60581dbb064
abdusalom <boriboyevabdusalom@gmail.com>
2023-11-23 20:12:52 +0500

    8

--- List08.py
+++ List08.py
@@ -6,4 +6,15 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    i=0
+    k=0
+    while i<len(list1):
+        if list1[i]==0:
+            list1[i]=False
+        i+=1
+        if list1[k]==1:
+            list1[k]=True
+        k+=1
+    return list1
+print(main([1, 0, 0, 0, 0]))
+print(main([1, 0, 1, 1, 0]))
